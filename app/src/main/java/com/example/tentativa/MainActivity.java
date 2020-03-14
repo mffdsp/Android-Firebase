3d187c10fbea87e18e17e7220c4926d4028f4199
mffdsp <mffdsp@ic.ufal.br>
2020-03-13 23:41:40 -0300

    commit 1

---
+++ app/src/main/java/com/example/tentativa/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.tentativa;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.tentativa.Data;
+import com.google.firebase.FirebaseError;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import android.app.Notification;
+import android.content.ContentValues;
+import android.content.SyncStatusObserver;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.ArrayMap;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class MainActivity<mFirebaseRef> extends AppCompatActivity {
+
+    String FINAL_NAME = "oo";
+    DatabaseReference DB;
+    DatabaseReference DR;
+    List<Pessoa> listaDePessoas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DB = FirebaseDatabase.getInstance().getReference("name");
+
+        listaDePessoas = new ArrayList<>();
+
+        getSupportActionBar().hide();
+    }
+
+
+    public void saveToDB(View v){
+
+        String name = ((EditText) findViewById(R.id.editText3)).getText().toString();
+        String data = ((EditText) findViewById(R.id.datatext)).getText().toString();
+
+        Pessoa pessoa = new Pessoa(name, data);
+        String id = DB.push().getKey();
+
+        DB.child(id).setValue(pessoa);
+
+        Toast.makeText(this, "SALVANDINHO", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        DB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                listaDePessoas.clear();
+
+                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                    Pessoa post = postSnapshot.getValue(Pessoa.class);
+                    listaDePessoas.add(post);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public void loadDB(View v){
+
+        for(Pessoa pessoa : listaDePessoas){
+            System.out.println(pessoa.nome + " " + pessoa.data);
+        }
+    }
+
+}
+
---
+++ app/src/main/java/com/example/tentativa/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.tentativa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Mudar cor
+        getWindow().getDecorView().setBackgroundColor(Color.rgb(255,69, 0));
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
